d3edbd4cef9b50bf37158b5230c37ed752978f90
lineeam <72556262+lineeam@users.noreply.github.com>
2023-04-21 06:49:08 +0200

    Add main GUI pages
    - Add Map page
    - Add Forecast page
    - Add Historical Weather page
    - Add details page
    - Add settings page
    - Add grid structure
    - Implement nav across pages
    - Implement unfinished navigation across pages (only map nav functional)

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,30 +28,44 @@ namespace WeatherApp
             this.InitializeComponent();
         }
 
+        //TO DO: Figure out why two of the cases throw errors
         private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-            /*if (args.IsSettingsSelected)
+            if (args.IsSettingsInvoked)
             {
                 //  to the app settings page
-                Frame.Navigate(typeof(SettingsPage));
-            }*//*
-            else
-            {*/
-                // Navigate to the selected page based on the Tag property
-            string tag = args.InvokedItemContainer.Tag.ToString();
-            switch (tag)
-            {
-                case "Map":
-                    Frame.Navigate(typeof(Map));
-                    break;
-                case "Details":
-                    Frame.Navigate(typeof(LibraryPage));
-                    break;
-                case "Forecast":
-                    Frame.Navigate(typeof(SettingsPage));
-                    break;
+                Frame.Navigate(typeof(Settings));
             }
-            
+            else
+            {
+                // Navigate to the selected page based on the Tag property
+                string tag = args.InvokedItemContainer.Tag.ToString();
+                switch (tag)
+                {
+                    case "Map":
+                        Frame.Navigate(typeof(Map));
+                        break;
+                    case "Details":
+                        Frame.Navigate(typeof(Details));
+                        break;
+                    case "Forecast":
+                        Frame.Navigate(typeof(Forecast));
+                        break;
+                    case "Historical":
+                        Frame.Navigate(typeof(Historical));
+                        break;
+                }
+
+            }
+        }
+
+        private void CitySearchBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                Console.WriteLine(CitySearchBox.Text);
+                //TO DO: Code to save city to global variable
+            }
         }
     }
 }
