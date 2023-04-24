27b8cc9d9f4cacba652dea2b6bf580921a6e1407
lineeam <72556262+lineeam@users.noreply.github.com>
2023-04-24 16:27:37 +0200

    Forecast gui (#6)
    - Add lock to search event method
    - Add method comments
    - Add Forecast Main Temp
    - Implement toggle button to change units
    - Update Settings page styling
    - Add forecast decription
    - Add multithreading button (no implementation yet)

--- WeatherApp/Forecast.xaml.cs
+++ WeatherApp/Forecast.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,82 @@ namespace WeatherApp
         public Forecast()
         {
             this.InitializeComponent();
+            WeakReferenceMessenger.Default.Register<NavSearch>(this, OnSearchTermReceived);
         }
+
+        private void OnSearchTermReceived(object recipient, NavSearch message)
+        {
+            lock (Global_Variables.lockObj)
+            {
+                getForecastData();
+            }
+        }
+
+        private async void getForecastData()
+        {
+            WeatherForecastData weatherForecastData = await ApiCalls.fetchFiveDaysWeatherForecast();
+
+            if(weatherForecastData != null)
+            {
+                populateDays(weatherForecastData);
+            }
+        }
+        
+        // TO DO: Add DB insert part
+        private void populateDays(WeatherForecastData forecast)
+        {/*
+            DbManager dbManager = DbManager.getInstance();
+
+            if (Global_Variables.isMultiThreaded)
+            {
+                dbManager.insertMulti(weatherForecastData);
+            }
+            else
+            {
+                dbManager.insertSingle(weatherForecastData.list);
+            }*/
+
+            for(int i  = 0; i <= 4; i++)
+            {
+                WeatherData.Root weather = forecast.list[i];
+                List<TextBlock> blockList = selectTextBlock(i);
+                if (blockList != null)
+                {
+                    blockList[0].Text = Math.Round(weather.main.temp).ToString() + (Global_Variables.units == "metric" ? "\u2103" : "\u2109");
+                    blockList[1].Text = weather.weather[0].description;
+                }
+            }
+        }
+
+        private List<TextBlock> selectTextBlock(int i)
+        {
+            List<TextBlock> list = new List<TextBlock>();
+            
+            switch (i)
+            {
+                case 0:
+                    list.Add(OneTemp);
+                    list.Add(OneDesc);
+                    return list;
+                case 1:
+                    list.Add(TwoTemp);
+                    list.Add(TwoDesc);
+                    return list;
+                case 2:
+                    list.Add(ThreeTemp);
+                    list.Add(ThreeDesc);
+                    return list;
+                case 3:
+                    list.Add(FourTemp);
+                    list.Add(FourDesc);
+                    return list;
+                case 4:
+                    list.Add(FiveTemp);
+                    list.Add(FiveDesc);
+                    return list;
+            }
+            return list;
+        }
+
     }
 }
