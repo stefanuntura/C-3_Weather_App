806201974a8e199656137e4c51e8f0f1bdcdf990
stefanuntura <56734208+stefanuntura@users.noreply.github.com>
2023-04-28 12:51:48 +0200

    UI improvements and historical data (#11)
    * Add data and images on forecast tab
    
    - Add data and images on forecast tab
    - Reduce corner radius of each individual card in forecast tab
    - Display city name above cards
    - Load info on load of tab and on search of city
    
    * Remove console logs from api calls and add api call for weather icon
    
    - Response type is wrong for weather api icon call
    
    * Add historical data tab UI and backend code for single and mutithreade options with semaphores. Implement Settings switch buttons functionaity and refactor name
    
    * UI improvements and historical data thread to main join

--- WeatherApp/Forecast.xaml.cs
+++ WeatherApp/Forecast.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
@@ -29,11 +22,21 @@ namespace WeatherApp
             WeakReferenceMessenger.Default.Register<NavSearch>(this, OnSearchTermReceived);
         }
 
+        private void NavBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            lock (Global_Variables.lockObj)
+            {
+                getForecastData();
+                CityName.Text = Global_Variables.cityName;
+            }
+        }
+
         private void OnSearchTermReceived(object recipient, NavSearch message)
         {
             lock (Global_Variables.lockObj)
             {
                 getForecastData();
+                CityName.Text = Global_Variables.cityName;
             }
         }
 
@@ -65,10 +68,42 @@ namespace WeatherApp
             {
                 WeatherData.Root weather = forecast.list[i];
                 List<TextBlock> blockList = selectTextBlock(i);
-                if (blockList != null)
+                if(blockList != null)
                 {
-                    blockList[0].Text = Math.Round(weather.main.temp).ToString() + (Global_Variables.units == "metric" ? "\u2103" : "\u2109");
+                    //Description of weather
                     blockList[1].Text = weather.weather[0].description;
+
+                    //Temperatures
+                    blockList[0].Text = Utilities.prepareTempForUI(weather.main.temp);
+                    blockList[2].Text = "Feels like: " + Utilities.prepareTempForUI(weather.main.feels_like);
+
+                    //Pressure
+                    blockList[3].Text = "Pressure: " + weather.main.pressure.ToString();
+
+                    //Humidity
+                    blockList[4].Text = "Humidity: " + weather.main.humidity.ToString();
+
+                    BitmapImage bitmap = new BitmapImage(new Uri(Utilities.prepareWeatherIconUrl(weather.weather[0].icon)));
+                    ImageSource imageSource = bitmap as ImageSource;
+
+                    switch (i)
+                    {
+                        case 0:
+                            OneImage.Source = imageSource;
+                            break;
+                        case 1:
+                            TwoImage.Source = imageSource;
+                            break;
+                        case 2:
+                            ThreeImage.Source = imageSource;
+                            break;
+                        case 3:
+                            FourImage.Source = imageSource;
+                            break;
+                        case 4:
+                            FiveImage.Source = imageSource;
+                            break;
+                    }
                 }
             }
         }
@@ -82,26 +117,40 @@ namespace WeatherApp
                 case 0:
                     list.Add(OneTemp);
                     list.Add(OneDesc);
+                    list.Add(OneFeelsLike);
+                    list.Add(OnePressure);
+                    list.Add(OneHumidity);
                     return list;
                 case 1:
                     list.Add(TwoTemp);
                     list.Add(TwoDesc);
+                    list.Add(TwoFeelsLike);
+                    list.Add(TwoPressure);
+                    list.Add(TwoHumidity);
                     return list;
                 case 2:
                     list.Add(ThreeTemp);
                     list.Add(ThreeDesc);
+                    list.Add(ThreeFeelsLike);
+                    list.Add(ThreePressure);
+                    list.Add(ThreeHumidity);
                     return list;
                 case 3:
                     list.Add(FourTemp);
                     list.Add(FourDesc);
+                    list.Add(FourFeelsLike);
+                    list.Add(FourPressure);
+                    list.Add(FourHumidity);
                     return list;
                 case 4:
                     list.Add(FiveTemp);
                     list.Add(FiveDesc);
+                    list.Add(FiveFeelsLike);
+                    list.Add(FivePressure);
+                    list.Add(FiveHumidity);
                     return list;
             }
             return list;
         }
-
     }
 }
